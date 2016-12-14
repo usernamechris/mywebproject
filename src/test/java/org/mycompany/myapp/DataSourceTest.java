11a16ae9a668a4db3798b93baa136e4cabd2e5f3
seok.han <seok.han@vovioskorea.com>
2016-12-14 16:35:57 +0900

    sample controller test 추가

--- src/test/java/org/mycompany/myapp/DataSourceTest.java
+++ src/test/java/org/mycompany/myapp/DataSourceTest.java
@@ -6,15 +6,8 @@ import javax.inject.Inject;
 import javax.sql.DataSource;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-// @RunWith, @ContextConfiguratio은 현재 테스트 코드 실행시 스프링이 로딩되로록 함
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-		locations ={"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
-public class DataSourceTest {
+public class DataSourceTest extends TestConfig {
 
 	@Inject
 	private DataSource ds;
--- src/test/java/org/mycompany/myapp/MyBatisTest.java
+++ src/test/java/org/mycompany/myapp/MyBatisTest.java
@@ -1,21 +1,10 @@
 package org.mycompany.myapp;
 
-import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-		locations ={"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
-public class MyBatisTest {
-	
-	@Inject
-	private SqlSessionFactory sqlFactory; // root-context에 정의
+public class MyBatisTest extends TestConfig {
 	
 	@Test
 	public void testFActory() {
