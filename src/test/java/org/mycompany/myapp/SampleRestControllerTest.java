48b0d0fc99cc123845ba520b0071db8bd8fc3650
seok.han <seok.han@vovioskorea.com>
2016-12-14 17:33:00 +0900

    add

--- src/test/java/org/mycompany/myapp/SampleRestControllerTest.java
+++ src/test/java/org/mycompany/myapp/SampleRestControllerTest.java
@@ -3,8 +3,6 @@ package org.mycompany.myapp;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.Test;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
@@ -13,12 +11,12 @@ public class SampleRestControllerTest extends TestConfig {
 
 	@Test
 	public void restTest() throws Exception {
-		ResultActions resutlAction = mockMvc.perform(MockMvcRequestBuilders.get(URL + "hello"))
+		mockMvc.perform(MockMvcRequestBuilders.get(URL + "hello"))
 				.andDo(MockMvcResultHandlers.print())
 				.andExpect(status().isOk())
 				.andExpect(content().string("Hello World"));
 		
-		resutlAction = mockMvc.perform(MockMvcRequestBuilders.get(URL + "sendVO"))
+		mockMvc.perform(MockMvcRequestBuilders.get(URL + "sendVO"))
 				.andDo(MockMvcResultHandlers.print())
 				.andExpect(status().isOk())
 				.andExpect(content().contentType("application/json;charset=UTF-8"))
@@ -26,7 +24,7 @@ public class SampleRestControllerTest extends TestConfig {
 				.andExpect(jsonPath("$.firstName").value("길동"))
 				.andExpect(jsonPath("$.lastName").value("홍"));
 		
-		resutlAction = mockMvc.perform(MockMvcRequestBuilders.get(URL + "sendList"))
+		mockMvc.perform(MockMvcRequestBuilders.get(URL + "sendList"))
 				.andDo(MockMvcResultHandlers.print())
 				.andExpect(status().isOk())
 				.andExpect(content().contentType("application/json;charset=UTF-8"));
