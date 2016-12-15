84b89e6b151ba26a2d3802d6d2f80c2d5eb8dd73
seok.han <seok.han@vovioskorea.com>
2016-12-15 09:47:49 +0900

    add test

--- src/test/java/org/mycompany/myapp/MyBatisTest.java
+++ src/test/java/org/mycompany/myapp/MyBatisTest.java
@@ -9,15 +9,16 @@ import org.junit.Test;
 public class MyBatisTest extends TestConfig {
 	
 	@Test
-	public void testFActory() {
+	public void testFactory() {
 		System.out.println("##factory: " + sqlFactory);
+		assertTrue(sqlFactory.toString().contains("DefaultSqlSessionFactory@"));
 	}
 
 	@Test
 	public void testSession() throws Exception {
 		try(SqlSession session = sqlFactory.openSession()) {
 			System.out.println("##session: " + session);
-			assertTrue(session.toString().contains("DefaultSqlSessionFactory@"));
+			assertTrue(session.toString().contains("DefaultSqlSession@"));
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
